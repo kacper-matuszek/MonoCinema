0167c9a6d5d5261c395367d896c1e634411eebbf
Kacper096 <kacpermatuszek2@gmail.com>
2022-05-21 19:51:31 +0200

    Add SharedKernel project. Add Aggregate root.

---
+++ src/BuildingBlocks/MonoCinema.SharedKernel/AggregateRoot.cs
@@ -0,0 +1,20 @@
+﻿namespace MonoCinema.SharedKernel;
+
+public abstract class AggregateRoot<TId>
+{
+    private bool _versionIncremented = false;
+
+    public TId Id { get; protected set; } = default!;
+    public int Version { get;protected set; }
+
+    protected void IncrementVersion()
+    {
+        if (_versionIncremented)
+        {
+            return;
+        }
+
+        Version++;
+        _versionIncremented = true;
+    }
+}
