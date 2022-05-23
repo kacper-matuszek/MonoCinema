60063fb96ac690056d6a912f5e6eb8007427e0f2
Kacper096 <kacpermatuszek2@gmail.com>
2022-05-23 21:36:21 +0200

    - Init aggregate version

--- src/BuildingBlocks/MonoCinema.SharedKernel/AggregateRoot.cs
+++ src/BuildingBlocks/MonoCinema.SharedKernel/AggregateRoot.cs
@@ -5,7 +5,7 @@ public abstract class AggregateRoot<TId>
     private bool _versionIncremented = false;
 
     public TId Id { get; protected set; } = default!;
-    public int Version { get;protected set; }
+    public int Version { get; protected set; } = 1;
 
     protected void IncrementVersion()
     {
